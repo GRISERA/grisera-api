1033c2e2e671002c835df9e25861b18c422c0826
Maczomen <maczomen2000@gmail.com>
2024-05-06 19:04:21 +0200

    feat: Add GitHub actions

---
+++ setup_dev.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import subprocess
+
+cf_remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in cf_remote_version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = cf_remote_version.split("-")
+    cf_remote_version = v + "+" + i + ".git." + s
+
+DESCRIPTION = 'Grisera-api package'
+LONG_DESCRIPTION = 'Graph Representation Integrating Signals for Emotion Recognition and Analysis (GRISERA) framework provides a persistent model for storing integrated signals and methods for its creation.'
+
+# Setting up
+setup(
+    name="grisera",
+    version=cf_remote_version,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'fastapi~=0.94.1',
+        'uvicorn[standard]',
+        'requests~=2.28.2',
+        'fastapi-utils',
+        'pydantic~=1.10.6',
+        'starlette~=0.26.1'
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
