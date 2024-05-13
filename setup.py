23da4176e97169f5a2d590d3bc53265d86884c62
Maczomen <maczomen2000@gmail.com>
2024-05-13 23:48:16 +0200

    fix: version change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 DESCRIPTION = 'Grisera-api package'
 LONG_DESCRIPTION = 'Graph Representation Integrating Signals for Emotion Recognition and Analysis (GRISERA) framework provides a persistent model for storing integrated signals and methods for its creation.'
 
