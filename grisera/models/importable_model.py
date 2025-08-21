e40c6cfe0d200388b790e96f6d6c6e79cafd8967
barbara.rosochacka <barbara.rosochacka@wp.pl>
2025-08-22 00:24:39 +0200

    wstępne dodanie importu

---
+++ grisera/models/importable_model.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ImportableModel(BaseModel):
+    """
+    Base model for entities that can be imported from external sources
+    
+    Attributes:
+        external_id (Optional[str]): External ID from import source (e.g. @id from JSON)
+    """
+    external_id: Optional[str] = None
+
+
